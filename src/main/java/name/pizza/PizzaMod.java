f1178aed29b770b2bfe49304fc92bdbc08f268bb
marshvmaro <m.a.r.s.h@outlook.com>
2026-08-11 16:03:47 -0500

    Add files via upload

---
+++ src/main/java/name/pizza/PizzaMod.java
@@ -0,0 +1,19 @@
+package name.pizza;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PizzaMod implements ModInitializer {
+	public static final String MOD_ID = "pizza-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
